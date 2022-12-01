e231bced4332524fef9b6709fe0f7cbb3abf5146
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 19:22:10 +0530

    refactor & find max

---
+++ Refactor1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxGeneric
+{
+    public class Refactor1
+    {
+        public static T findmax<T>(T first, T second, T third) where T : IComparable<T>
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && first.CompareTo(first) > 0 ||
+                third.CompareTo(second) >= 0 && first.CompareTo(first) > 0 ||
+                third.CompareTo(second) > 0 && first.CompareTo(first) >= 0)
+            {
+                return third;
+            }
+            return first;
+        }
+    }
+}
