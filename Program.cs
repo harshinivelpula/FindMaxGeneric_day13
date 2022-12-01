4cfb209eec4c0149c247829cb089a0ca5ff9c8d5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 19:22:47 +0530

    refactor & find max

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("find max number");
+
+            Console.WriteLine("Maximum Integer Value:" + Refactor1.findmax<int>(3, 8, 11));
+            Console.WriteLine("Maximum Float Value:" + Refactor1.findmax<float>(1.11f, 3.33f, 4.44f));
+            Console.WriteLine("Maximum String Value:" + Refactor1.findmax<string>("aa", "bb", "cc"));
         }
     }
 }
