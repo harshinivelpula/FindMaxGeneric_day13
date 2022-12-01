d48ad4418d20d0f0fac182c771225864fc877755
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 19:33:25 +0530

    extending max method to take morethan 3 parameters

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("find max number");
+            Console.WriteLine("Maximum Integer Number is: ");
+            int output = ExtendMax.MaximumIntegerNumber(8, 11, 28, 6);
+            Console.WriteLine(output);
+            Console.WriteLine("Maximum Float Number is:");
+            float outputs = ExtendMax.MaximumFloatNumber(33.33f, 40.40f, 80.28f, 11.60f);
+            Console.WriteLine(output);
+            Console.WriteLine("Maximum String value is : ");
+            string strigoutput = ExtendMax.MaximumStringNumber("Apple", "Peach", "Banana", "Grapes");
+            Console.WriteLine(strigoutput);
         }
     }
 }
