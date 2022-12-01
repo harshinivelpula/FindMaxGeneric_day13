dda017e823bb0cb1deeb56249ce305337f7b3ccd
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 19:38:52 +0530

    extend method using generic

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("find max number");
+            Console.WriteLine("Find the maximum number: ");
+            int[] arr = { 111, 222, 333, 444, 555 };
+            ExtendGeneric<int> generic = new ExtendGeneric<int>(arr);
+            generic.PrintMaxValue();
+            double[] doublearr = { 11.1, 22.2, 33.3, 44.4, 55.5 };
+            ExtendGeneric<double> genericDouble = new ExtendGeneric<double>(doublearr);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "666", "777", "888", "999", "000" };
+            ExtendGeneric<string> genericString = new ExtendGeneric<string>(stringArray);
+            genericString.PrintMaxValue();
         }
     }
 }
