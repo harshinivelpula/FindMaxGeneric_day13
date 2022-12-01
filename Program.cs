c650ec4ef9ce38be9483581f418c8b721db1a9bc
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 19:05:36 +0530

    given 3 float to find max

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("find max number");
+            float output = @float.MaximumFloatNumber(111, 2, 22);
+            Console.WriteLine(output);
         }
     }
 }
