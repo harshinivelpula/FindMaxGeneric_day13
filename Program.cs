8dd0851f7b0cc609daf94c361e0eeaa764ced602
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-01 18:53:55 +0530

    welcome to find max using generics

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaxGeneric
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("find max number");
+        }
+    }
+}
